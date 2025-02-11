6432a78ba281b148b15c2cea0cf73a233975796b
Mauricio Gonzalez <86858563+MauricioGonzal@users.noreply.github.com>
2025-02-11 00:39:20 -0300

    Se modifica ExerciseDetails para poder permitir la combinacion de ejercicios

--- PuraEsencia/src/main/java/com/aplicaciongimnasio/PuraEsencia/model/ExerciseDetails.java
+++ PuraEsencia/src/main/java/com/aplicaciongimnasio/PuraEsencia/model/ExerciseDetails.java
@@ -3,12 +3,13 @@ package com.aplicaciongimnasio.PuraEsencia.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class ExerciseDetails {
-    private Long exerciseId;  // ID del ejercicio (relación con Exercise)
-    private String name;
-    private String description;
-    private int series;       // Número de series
-    private int repetitions;  // Número de repeticiones
+    private List<Long> exerciseIds;  // ID del ejercicio (relación con Exercise)
+    private Byte series;       // Número de series
+    private Byte repetitions;  // Número de repeticiones
+    private Byte rest;
 }
